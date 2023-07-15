e4edd299e110aa2b245099b713318efc70170ec8
Amy Sawyer <amyleighsawyer97@gmail.com>
2023-07-15 22:20:57 +0000

    Added input to ask for users name and replaced anything player related with their name.

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ import random
 BOARD_SIZE = 5
 NUM_SHIPS = 4
 
+# Get the player's name
+player_name = input("Please enter your name: ")
+
 # Create the game board
 player_board = [['O' for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
 computer_board = [['O' for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
@@ -20,7 +23,7 @@ for _ in range(NUM_SHIPS):
 
 # Function to print the game boards
 def print_boards(player_board, computer_board):
-    print("Player's Board:")
+    print(player_name + "'s Board:")
     for row in player_board:
         print(" ".join(row))
     print("\nComputer's Board:")
@@ -44,10 +47,10 @@ for ship_row, ship_col in player_ships:
 # Game loop
 for turn in range(1):  # Ten turns to guess
     print("Turn", turn + 1)
-    
+
     # Print the game boards
     print_boards(player_board, computer_board)
-    
+
     valid_guess = False
     while not valid_guess:
         # Ask the player for a guess
@@ -57,13 +60,13 @@ for turn in range(1):  # Ten turns to guess
         except ValueError:
             print("Error: Invalid guess. Please enter integer values.")
             continue
-        
+
         # Validate the player's guess
         valid_guess = validate_input(guess_row, guess_col)
-    
+
     # Check if the guess is correct
     if (guess_row, guess_col) in computer_ships:
-        print("Congratulations! You sunk a battleship!")
+        print("Congratulations,", player_name + "! You sunk a battleship!")
         computer_board[guess_row][guess_col] = '@'
         computer_ships.remove((guess_row, guess_col))
     elif computer_board[guess_row][guess_col] == 'X':
@@ -71,23 +74,23 @@ for turn in range(1):  # Ten turns to guess
     else:
         print("You missed.")
         computer_board[guess_row][guess_col] = 'X'
-    
+
     # Computer's turn
     computer_guess_row = random.randint(0, BOARD_SIZE - 1)
     computer_guess_col = random.randint(0, BOARD_SIZE - 1)
-    
+
     # Check if the computer's guess is correct
     if (computer_guess_row, computer_guess_col) in player_ships:
-        print("Oh no! The computer sunk one of your battleships!")
+        print("Oh no,", player_name + "! The computer sunk one of your battleships!")
         player_board[computer_guess_row][computer_guess_col] = '@'
         player_ships.remove((computer_guess_row, computer_guess_col))
     else:
         print("The computer missed.")
         player_board[computer_guess_row][computer_guess_col] = 'X'
-    
+
     # Check if all player's ships have been sunk
     if len(player_ships) == 0:
-        print("Oh no! The computer sunk all your battleships!")
+        print("Oh no,", player_name + "! The computer sunk all your battleships!")
         break
 
 # Print the final game boards
